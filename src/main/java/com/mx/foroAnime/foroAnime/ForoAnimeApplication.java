84378f307c7aa0d243c57fec675ac48e1e43faff
UrielPCDesk <floresruizur13l@outlook.com>
2023-10-07 16:30:04 -0700

    Se creo el repositorio

---
+++ src/main/java/com/mx/foroAnime/foroAnime/ForoAnimeApplication.java
@@ -0,0 +1,13 @@
+package com.mx.foroAnime.foroAnime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForoAnimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForoAnimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mx/foroAnime/foroAnime/ForoAnimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mx.foroAnime.foroAnime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForoAnimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
